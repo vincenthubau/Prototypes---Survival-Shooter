fec2097d3011b5d95ee8d3da6206a393305d2ab8
Vincent Hubau <vincent.hubau@gmail.com>
2015-01-19 17:31:58 -0500

    Data added

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+	public float restartDelay = 5f;         // Time to wait before restarting the level
+
+    Animator anim;
+	float restartTimer;                     // Timer to count up to restarting the level
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+			restartTimer += Time.deltaTime;
+			if(restartTimer >= restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+        }
+    }
+}
